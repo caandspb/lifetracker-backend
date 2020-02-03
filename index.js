478f8de1e4a8d5b6f484c26090a11681d6dce590
Sergey Kobzev <skobzev@ephasoft.com>
2020-02-04 01:00:55 +0300

    Setup mongoose, add userModel

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
+
 const app = express();
 const port = '3000';
+
+mongoose.connect(process.env.DB_CONNECTION_STRING,  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on('error', () => {
+    console.log('Connection error');
+});
+
+db.on('open', () => {
+    console.log('Connection open');
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
